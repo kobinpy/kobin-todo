9ad4aae44410931b7660fe62446dab3278f63e1b
c-bata <contact@c-bata.link>
2016-04-19 22:58:37 +0900

    Modify because API changes

--- app/views/auth.py
+++ app/views/auth.py
@@ -4,7 +4,7 @@ from ..service import github as github_service
 
 
 def github_oauth_callback():
-    code = request.GET['code'].value
+    code = request.GET['code']
     payload = {
         "client_id": app.config.get('GITHUB_CLIENT_ID'),
         "client_secret": app.config.get('GITHUB_CLIENT_SECRET'),
