2c449f16e23fbd27847c2993509972be0e3e5c7f
c-bata <contact@c-bata.link>
2016-04-22 08:55:32 +0900

    Update layout

--- static/ts/task-list.component.ts
+++ static/ts/task-list.component.ts
@@ -7,45 +7,61 @@ import {TaskDetailComponent} from './task-detail.component'
 @Component({
     selector: 'app-task-list',
     template: `
-        <header>
-            <div class="top">
-                <img class="logo" src="/static/img/kobin.png">
-                <div class="task-add form-inline">
-                    <div class="form-group">
-                        <input #newTask class="form-control" />
-                    </div>
-                    <button class="btn btn-default" (click)="addTask(newTask.value); newTask.value=''">ADD</button>
-                </div>
-                
-                <div class="alert alert-danger" role="alert" *ngIf="errorMessage">
-                    <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                    <span class="sr-only">Error: </span>
-                    {{errorMessage}}
-                </div>
+    
+    <div class="row task-view">
+        <div class="task-list col-sm-4">
+            <p>Progress: {{ doneTaskLength() }} / {{ tasks.length }}</p>
             
-                <ul>
-                    <li [class.selected]="task === selectedTask" *ngFor="#task of tasks" (click)="onSelect(task)">
-                        <input type="checkbox" (click)="toggleDone(task)" [checked]="task.done"> {{task.title}}
-                    </li>
-                </ul>
-            </div>
-            <div class="bottom">
-                <a href="https://github.com/login/oauth/authorize?client_id=9fbe39b8584d366afd85">Github Auth</a>
-                <p>Progress: {{ doneTaskLength() }} / {{ tasks.length }}</p>
-                <p>Powered by Kobin</p>
-            </div>
-        </header>
-        
-        <main>
+            <ul>
+                <li [class.selected]="task === selectedTask" *ngFor="#task of tasks" (click)="onSelect(task)">
+                    <input type="checkbox" (click)="toggleDone(task)" [checked]="task.done"> {{task.title}}
+                </li>
+            </ul>
+        </div>
+        <div class="contents col-sm-8">
             <app-task-detail [task]="selectedTask" [tasks]="tasks"></app-task-detail>
-        </main>
+        </div>
+    </div>
+    <div class="task-add form-inline">
+        <div class="form-group">
+            <label>New Task: </label>
+            <input #newTask class="form-control" />
+        </div>
+        <button class="btn btn-default" (click)="addTask(newTask.value); newTask.value=''">Task Add</button>
+        <div class="form-group alert alert-danger" role="alert" *ngIf="errorMessage">
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <span class="sr-only">Error: </span>
+            {{errorMessage}}
+        </div>
+    </div>
     `,
     providers: [TaskService],
     directives: [TaskDetailComponent],
     styles: [`
+        .sidebar {
+            color: #222;
+            background-color: #fff;
+        }
+        .task-list {}
+        .row { margin: 0; }
+        .alert { padding: 6px; }
+        .task-add {
+            position: fixed;
+            top: 0;
+            left: 300px;
+            width: 100%;
+            height: 60px;
+            
+            background-color: #fff;
+            border-bottom: 1px solid #ddd;
+        }
+        .task-view {
+            position: static;
+            margin-top: 61px;
+        }
         li { list-style: none; padding: 10px 0 10px 20px; }
         ul { padding-left: 0px; }
-        .selected { background-color: #2980b9; }
+        .selected { background-color: #ddd; }
         .task-add { padding: 10px 10px 10px 20px; }
     `],
 })
