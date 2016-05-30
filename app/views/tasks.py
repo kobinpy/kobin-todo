e6a431d61645e1549dc38984dd49ebaced35d56f
c-bata <contact@c-bata.link>
2016-05-31 02:41:21 +0900

    Update header handling

--- app/views/tasks.py
+++ app/views/tasks.py
@@ -6,20 +6,20 @@ from ..service import task as task_service
 
 
 def task_list():
-    response.add_header('Content-Type', 'application/json; charset=utf-8')
+    response.headers.add_header('Content-Type', 'application/json; charset=utf-8')
     session = app.config["DB"]["SESSION"]
     tasks = [t.serialize for t in session.query(models.Task).all()]
     return json.dumps({'tasks': tasks})
 
 
 def add_task():
-    response.add_header('Content-Type', 'application/json; charset=utf-8')
+    response.headers.add_header('Content-Type', 'application/json; charset=utf-8')
     new_task = task_service.add_task(title=request.json['title'])
     return json.dumps(new_task.serialize)
 
 
 def get_task(task_id: int):
-    response.add_header('Content-Type', 'application/json; charset=utf-8')
+    response.headers.add_header('Content-Type', 'application/json; charset=utf-8')
     task = task_service.get_task(task_id)
     if task is None:
         raise HTTPError(404, "Not found: {}".format(repr(request.path)))
@@ -27,7 +27,7 @@ def get_task(task_id: int):
 
 
 def update_task(task_id: int):
-    response.add_header('Content-Type', 'application/json; charset=utf-8')
+    response.headers.add_header('Content-Type', 'application/json; charset=utf-8')
     updated_task = task_service.update_task(task_id, request.json['task'])
     return json.dumps(updated_task.serialize)
 
