7d8cb83b1ecf29ee76e840ea07390eb1ccd3fd8a
c-bata <contact@c-bata.link>
2016-12-04 01:56:13 +0900

    Update routing

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,11 +3,11 @@ def setup_routing(app):
     from . import auth
 
     # tasks
-    app.route('^/api/tasks$', 'GET', tasks.task_list)
-    app.route('^/api/tasks$', 'POST', tasks.add_task)
-    app.route('^/api/tasks/(?P<task_id>\d+)$', 'GET', tasks.add_task)
-    app.route('^/api/tasks/(?P<task_id>\d+)$', 'PATCH', tasks.update_task)
-    app.route('^/api/tasks/(?P<task_id>\d+)$', 'DELETE', tasks.delete_task)
+    app.route('/api/tasks', 'GET', tasks.task_list)
+    app.route('/api/tasks', 'POST', tasks.add_task)
+    app.route('/api/tasks/{task_id}', 'GET', tasks.add_task)
+    app.route('/api/tasks/{task_id}', 'PATCH', tasks.update_task)
+    app.route('/api/tasks/{task_id}', 'DELETE', tasks.delete_task)
 
     # auth
-    app.route('^/auth/github/callback$', 'GET', auth.github_oauth_callback)
+    app.route('/auth/github/callback', 'GET', auth.github_oauth_callback)
