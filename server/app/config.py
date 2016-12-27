bb8db0e97298dece5bd5e9d9ed9e5f718c9bc5b2
c-bata <contact@c-bata.link>
2016-12-28 02:54:14 +0900

    Update environment

--- server/app/config.py
+++ server/app/config.py
@@ -27,15 +27,15 @@ elif ENV == 'test':
     SECRET_KEY = b'secretkey'
 elif ENV == 'docker':
     DEBUG = False
-    SECRET_KEY = os.environ.get('KOBIN_TODO_SECRET_KEY').encode('utf-8')
+    SECRET_KEY = os.environ.get('SECRET_KEY').encode('utf-8')
 
-    REDIS_HOST = os.environ.get('KOBIN_TODO_REDIS_HOST')
-    REDIS_PORT = os.environ.get('KOBIN_TODO_REDIS_PORT')
-    REDIS_PASSWORD = os.environ.get('KOBIN_TODO_REDIS_PASSWORD')
-    REDIS_DB = os.environ.get('KOBIN_TODO_REDIS_DB')
+    REDIS_HOST = os.environ.get('REDIS_HOST')
+    REDIS_PORT = os.environ.get('REDIS_PORT')
+    REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+    REDIS_DB = os.environ.get('REDIS_DB')
 
-    host = os.environ.get('KOBIN_TODO_DATABASE_HOST')
-    user = os.environ.get('KOBIN_TODO_DATABASE_USER')
-    password = os.environ.get('KOBIN_TODO_DATABASE_PASSWORD')
-    db = os.environ.get('KOBIN_TODO_DATABASE_NAME')
+    host = os.environ.get('POSTGRES_HOST')
+    user = os.environ.get('POSTGRES_USER')
+    password = os.environ.get('POSTGRES_PASSWORD')
+    db = os.environ.get('POSTGRES_NAME')
     SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{user}:{password}@{host}/{db}'
